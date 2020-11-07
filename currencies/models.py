cfbf7de2ba57023905eac8eef160992f0f29ef04
ssabuda <slawek.sabuda@gmail.com>
2020-11-07 17:21:30 +0100

    add model Rate with manager and migration

--- currencies/models.py
+++ currencies/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+class RateManager(models.Manager):
+    def newest(self, **kwargs):
+        newest_date = self.latest("date").date
+        return self.filter(data=newest_date)
+
+
+class Rate(TimeStampedModel):
+    currency = models.CharField(max_length=3, null=False, blank=False, db_index=True)
+    date = models.DateField()
+    rate = models.DecimalField(max_digits=12, decimal_places=4, blank=False, null=False)
+
+    objects = RateManager()
+
+    class Meta:
+        verbose_name = "Currency rate"
+        verbose_name_plural = "Currency rates"
+        ordering = ("-date", "currency")
+        index_together = ("currency", "date")
+        unique_together = ("currency", "date")
+
+    def __str__(self) -> str:
+        return f"{self.date} - {self.currency} - {self.rate}"
