8694fee7a8abd0061b51573df707eb50fb02dc2d
ssabuda <slawek.sabuda@gmail.com>
2020-11-15 10:04:09 +0100

    add assert pk in test create TestRateFacade

--- currencies/tests/test_facade.py
+++ currencies/tests/test_facade.py
@@ -15,6 +15,10 @@ class TestRateFacade(TestEcbAdapterMixin):
         model = Rate.objects.get(currency=self.adapter.currency, date=self.adapter.date)
         # then
         self.assertEqual(model.rate, self.adapter.rate)
+        self.assertEqual(
+            model.pk,
+            Rate.objects.get(currency=self.adapter.currency, date=self.adapter.date).pk,
+        )
 
     def test_already_exists(self):
         # given
