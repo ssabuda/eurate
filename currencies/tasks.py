3f416ae981b7d1e8fc742df00645b995162de0ef
ssabuda <slawek.sabuda@gmail.com>
2020-11-17 22:21:20 +0100

    add tasks

---
+++ currencies/tasks.py
@@ -0,0 +1,26 @@
+import logging
+from typing import Optional
+
+from .adapter import RateEcbAdapter
+from .facade import RateFacade
+from .models import Rate
+from .repository import APIEcbRepository
+
+logger = logging.getLogger(__name__)
+
+
+def run_import(historic: Optional[bool] = False):
+    logger.info("Start task run_import historical=%s", historic)
+    for adapter in APIEcbRepository(historic).all():
+        create_rate(adapter)
+    logger.info("End task run_import")
+
+
+def create_rate(adapter: RateEcbAdapter):
+    if Rate.objects.is_exists(adapter.date, adapter.currency):
+        logger.info(
+            "Skipped exists Rate currency=%s date=%s", adapter.currency, adapter.date
+        )
+        return
+    RateFacade(adapter).create()
+    logger.info("Rate created currency=%s date=%s", adapter.currency, adapter.date)
