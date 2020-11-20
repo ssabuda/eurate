6d4befd4c38ed92fb39f2fa9bfdebbc3bae1744c
ssabuda <slawek.sabuda@gmail.com>
2020-11-20 19:57:29 +0100

    action export to csv on admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin
+import csv
+import datetime
 
-# Register your models here.
+from django.contrib import admin
+from django.http import HttpResponse, HttpRequest
+
+
+def export_to_csv(
+    modeladmin: admin.ModelAdmin, request: HttpRequest, queryset
+) -> HttpResponse:
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type="text/csv")
+    response[
+        "Content-Disposition"
+    ] = "attachment; \
+        filename={}.csv".format(
+        opts.verbose_name
+    )
+    writer = csv.writer(response)
+
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # Save first row with columnn name.
+    writer.writerow([field.verbose_name for field in fields])
+    # Save record sets.
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%Y-%m-%d %H:%M")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = "Export CSV"
--- currencies/admin.py
+++ currencies/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+
+from core.admin import export_to_csv
 from .models import Rate
 
 
 @admin.register(Rate)
 class RateAdmin(admin.ModelAdmin):
-    list_display = ["created", "modified", "date", "currency", "rate"]
+    list_display = ("created", "modified", "date", "currency", "rate")
+    list_filter = ("currency", "date")
+    actions = (export_to_csv,)
