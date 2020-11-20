3b29595c800b45cee3fcd7ff7984fcdf1c0c2801
ssabuda <slawek.sabuda@gmail.com>
2020-11-20 19:13:46 +0100

    add test create_rate

--- currencies/tests/test_tasks.py
+++ currencies/tests/test_tasks.py
@@ -1,7 +1,11 @@
 from unittest import mock
 from django.test import TestCase
 
+from .test_adapter import TestEcbAdapterMixin
+from ..facade import RateFacade
+from ..factories import RateFactoryUSD
 from ..fixtures import ECB_RESPONSE
+from ..models import Rate
 from ..repository import APIEcbRepository
 from ..tasks import create_rate, run_import
 
@@ -13,3 +17,21 @@ class TestRunImport(TestCase):
         mock_api.return_value = ECB_RESPONSE
         run_import()
         self.assertEqual(mock_create.call_count, 2048)
+
+
+class TestCreateRate(TestEcbAdapterMixin):
+    def test_create(self):
+        create_rate(self.adapter)
+        model = Rate.objects.get(currency=self.adapter.currency, date=self.adapter.date)
+        # then
+        self.assertEqual(model.rate, self.adapter.rate)
+        self.assertEqual(
+            model.pk,
+            Rate.objects.get(currency=self.adapter.currency, date=self.adapter.date).pk,
+        )
+
+    @mock.patch.object(RateFacade, "create")
+    def test_create_exists(self, mock_create):
+        RateFactoryUSD(date=self.adapter.date)
+        create_rate(self.adapter)
+        mock_create.assert_not_called()
