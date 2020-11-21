ff1fca6ac86f1c4e3a7ebaffae262ab7975c2c16
ssabuda <slawek.sabuda@gmail.com>
2020-11-21 11:24:54 +0100

    help text to run_ecb_import

--- currencies/management/commands/run_ecb_iport.py
+++ currencies/management/commands/run_ecb_iport.py
@@ -4,9 +4,17 @@ from currencies.tasks import run_import
 
 
 class Command(BaseCommand):
+    help = (
+        "Use this command for import currency exchange rate from European Central Bank"
+    )
+
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
-        parser.add_argument("--historic", action="store_true")
+        parser.add_argument(
+            "--historic",
+            action="store_true",
+            help="Add this argument if you want import full historic currency exchange rate",
+        )
 
     def handle(self, *args, **options):
         if options.get("historic"):
