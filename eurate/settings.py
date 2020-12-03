71db7d8e52ce4062347bb827bf82c052f0356c27
slawek <slawek.sabuda@gmail.com>
2020-12-03 21:24:44 +0100

    add api v1 app

--- eurate/settings.py
+++ eurate/settings.py
@@ -172,3 +172,9 @@ ECB_HISTORIC_RATES_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hi
 ECB_LAST_90_DAYS_RATES_URL = (
     "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
 )
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 100,
+}
