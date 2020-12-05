5671039b0705a6793ebf52947f2d91f0a47a0cf4
slawek <slawek.sabuda@gmail.com>
2020-12-05 15:27:15 +0100

    change db name settings for github actions

--- eurate/settings.py
+++ eurate/settings.py
@@ -120,7 +120,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": "eurate",
+            "NAME": "demo",
             "USER": "ctest",
             "PASSWORD": "coveragetest123",
             "HOST": "localhost",
