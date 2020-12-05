68b409730401039afdda0d900fdb0846937fc26b
ssabuda <49313958+ssabuda@users.noreply.github.com>
2020-12-05 17:33:46 +0100

    Revert "[WIP]feature/1-api"

--- api/v1/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from currencies.models import Rate
-
-
-class RateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Rate
-        fields = ("currency", "date", "rate")
