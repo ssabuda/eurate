dfd7cf5c687a339fa650273d362dcfb31aca0795
slawek <slawek.sabuda@gmail.com>
2020-12-05 16:15:13 +0100

    next try

--- eurate/settings.py
+++ eurate/settings.py
@@ -120,7 +120,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": "eurate",
+            "NAME": "postgres",
             "USER": "postgres",
             "PASSWORD": "postgres",
             "HOST": "localhost",
