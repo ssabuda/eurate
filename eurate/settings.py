84c9d613a524415ac9539567faebf4f82b6756e8
slawek <slawek.sabuda@gmail.com>
2020-12-05 16:02:13 +0100

    rename db

--- eurate/settings.py
+++ eurate/settings.py
@@ -120,7 +120,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": "postgres",
+            "NAME": "eurate",
             "USER": "postgres",
             "PASSWORD": "postgres",
             "HOST": "localhost",
