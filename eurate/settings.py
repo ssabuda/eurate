a3128a12d426eb40952f2e3d041074a9e8f943e3
slawek <slawek.sabuda@gmail.com>
2020-12-12 13:57:55 +0100

    add home page with bootstrap4

--- eurate/settings.py
+++ eurate/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "rest_framework",
     "django_filters",
+    "bootstrap4",
     # my app
     "core",
     "currencies",
@@ -166,6 +167,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 LOGGING = {
     "version": 1,
--- eurate/urls.py
+++ eurate/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from core.views import HomeTemplateView
+
 urlpatterns = [
+    path("", HomeTemplateView.as_view(), name="home"),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls", namespace="api")),
