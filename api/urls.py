a6e9bcc690cb89fb10f7a35146b0d5aae5f5327a
slawek <slawek.sabuda@gmail.com>
2020-12-12 12:49:22 +0100

    add api root view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from api.views import RateList, RateNewestList
+from api.views import RateList, RateNewestList, api_root
 
 app_name = "api"
 urlpatterns = [
+    path("", api_root, name="root"),
     path("v1/rates/", RateList.as_view(), name="rates"),
     path("v1/newest/", RateNewestList.as_view(), name="newest"),
 ]
